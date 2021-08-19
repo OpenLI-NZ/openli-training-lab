6647a8d542a5de54139e8c403f55f231828901b0
Shane Alcock <salcock@waikato.ac.nz>
2021-08-20 11:01:00 +1200

    Fix networking for lab containers
     * make sure there is a working "upstream" via the host
     * put openli-lab on each container as eth1
     * ensure both ends of mediator->agency link are connected

--- setup.sh
+++ setup.sh
@@ -5,13 +5,6 @@ docker network inspect openli-lab > /dev/null 2>&1 || \
         docker network create --driver bridge \
         openli-lab
 
-docker network inspect openli-upstream > /dev/null 2>&1 || \
-        docker network create --driver bridge \
-        -o "com.docker.network.bridge.enable_ip_masquerade=true" \
-        -o "com.docker.network.bridge.enable_icc=false" \
-        -o "com.docker.network.bridge.host_binding_ipv4=0.0.0.0" \
-        openli-upstream
-
 
 docker network inspect openli-agency > /dev/null 2>&1 || \
         docker network create --driver bridge \
@@ -51,23 +44,23 @@ echo "Downloading docker images..."
 echo "Images downloaded!"
 
 echo "Starting docker containers..."
-docker run -d -P --rm -it --name openli-agency --network=openli-agency openli/training:openli-lab-agency
+docker run -d -P --rm -it --name openli-agency openli/training:openli-lab-agency
 
-docker run -d -P --rm -it --name openli-provisioner  --network=openli-lab openli/training:openli-lab-provisioner
+docker run -d -P --rm -it --name openli-provisioner  openli/training:openli-lab-provisioner
 
-docker run -d -P --rm -it --name openli-mediator  --network=openli-lab openli/training:openli-lab-mediator
+docker run -d -P --rm -it --name openli-mediator openli/training:openli-lab-mediator
 
-docker run -d -P --rm -it --name openli-collector --network=openli-lab openli/training:openli-lab-collector
+docker run -d -P --rm -it --name openli-collector openli/training:openli-lab-collector
 
 
-docker network connect openli-upstream openli-collector
-docker network connect openli-upstream openli-mediator
-docker network connect openli-upstream openli-provisioner
-docker network connect openli-upstream openli-agency
+docker network connect openli-lab openli-provisioner
+docker network connect openli-lab openli-mediator
+docker network connect openli-lab openli-collector
 
 docker network connect openli-lab-replay openli-collector
 
 docker network connect openli-agency openli-mediator
+docker network connect openli-agency openli-agency
 echo "Containers started!"
 
 echo "OpenLI lab setup complete!"
